8938ee98889920024e03b1cc797349006fb24609
yangsijian <yangsijian2022@gmail.com>
2026-06-10 20:06:10 +0800

    更新 loon.js

--- loon.js
+++ loon.js
@@ -1,29 +1,23 @@
-let config = {
-  username: "", // 用户名
-  token: "", // token
-};
+const token = $prefs.valueForKey("sliverkiss_github_token");
 
-// load user prefs from box
-const boxConfig = $persistentStore.read("github_private_repo");
-if (boxConfig) {
-  config = JSON.parse(boxConfig);
-}
+(async () => {
+    // 构造 Raw URL
+    const req = {
+        url: `https://raw.githubusercontent.com${$request.path}`,
+        method: "GET",
+        headers: {
+            "Authorization": `token ${token}`,
+        }
+    };
 
-const match = $request.url.match(
-  /https:\/\/(?:raw|gist)\.githubusercontent\.com\/([^\/]+)\//
-);
+    $task.fetch(req).then((response) => {
+        $done({
+            status: `HTTP/1.1 200 OK`,
+            headers: {
+                "Content-Type": response.headers["Content-Type"] || "text/plain"
+            },
+            body: response?.body
+        });
+    })
 
-console.log("match =", JSON.stringify(match));
-
-if (!match) {
-  console.log("url not match");
-  $done({});
-  return;
-}
-
-const username = match[1];
-// rewrite headers for specific user
-if (username == config.username) {
-  console.log(`ACCESSING PRIVATE REPO: ${$request.url}`);
-  $done({ headers: {...$request.headers, Authorization: `token ${config.token}`} });
-} else $done({});
+})();
