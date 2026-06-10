7059a4bce33f6817c2bf7576e3f1fdb8356d5186
yangsijian <yangsijian2022@gmail.com>
2026-06-10 21:03:36 +0800

    更新 loon.js

--- loon.js
+++ loon.js
@@ -1,24 +1,22 @@
+console.log("$argument =", $argument);
+
 const args = Object.fromEntries(
   ($argument || "")
     .split("&")
     .filter(Boolean)
-    .map(i => {
-      const [k, ...v] = i.split("=");
-      return [k, decodeURIComponent(v.join("="))];
+    .map(item => {
+      const [key, ...value] = item.split("=");
+      return [key, decodeURIComponent(value.join("="))];
     })
 );
+
+console.log("args =", JSON.stringify(args));
 
 const USERNAME = args.USERNAME || "";
 const TOKEN = args.TOKEN || "";
 
 console.log("USERNAME =", USERNAME);
 console.log("TOKEN =", TOKEN ? "FOUND" : "EMPTY");
-
-if (!TOKEN || !USERNAME) {
-  console.log("Missing USERNAME or TOKEN");
-  $done({});
-  return;
-}
 
 const match = $request.url.match(
   /^https:\/\/(?:raw|gist)\.githubusercontent\.com\/([^\/]+)\//
@@ -32,15 +30,25 @@ if (!match) {
 
 const username = match[1];
 
+console.log("Request Username =", username);
+
+if (!USERNAME || !TOKEN) {
+  console.log("Missing USERNAME or TOKEN");
+  $done({});
+  return;
+}
+
 if (username === USERNAME) {
   console.log(`ACCESSING PRIVATE REPO: ${username}`);
 
-  $done({
-    headers: {
-      ...$request.headers,
-      Authorization: `token ${TOKEN}`
-    }
-  });
+  const headers = {
+    ...$request.headers,
+    Authorization: `token ${TOKEN}`
+  };
+
+  console.log("Authorization Added");
+
+  $done({ headers });
 } else {
   console.log(`Skip: ${username}`);
   $done({});
