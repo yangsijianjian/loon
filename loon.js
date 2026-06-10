9651b0679656405ab45da5cfc979b870c3ed2710
yangsijian <yangsijian2022@gmail.com>
2026-06-10 17:23:17 +0800

    更新 loon.js

--- loon.js
+++ loon.js
@@ -1,28 +1,12 @@
-console.log("===START===");
-console.log("$argument =", $argument);
-console.log("$request.url =", $request.url);
 const args = Object.fromEntries(
   ($argument || "")
     .split("&")
+    .filter(Boolean)
     .map(i => i.split("="))
 );
 
-const config = {
-  username: decodeURIComponent(args.USERNAME || ""),
-  token: decodeURIComponent(args.TOKEN || "")
-};
+const USERNAME = decodeURIComponent(args.USERNAME || "");
+const TOKEN = decodeURIComponent(args.TOKEN || "");
 
-const repoOwner = $request.url.match(
-  /https:\/\/(?:raw|gist)\.githubusercontent\.com\/([^\/]+)\//
-)?.[1];
-
-if (repoOwner === config.username) {
-  $done({
-    headers: {
-      ...$request.headers,
-      Authorization: `token ${config.token}`
-    }
-  });
-} else {
-  $done({});
-}
+console.log("USERNAME:", USERNAME);
+console.log("TOKEN:", TOKEN);
